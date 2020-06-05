257c43ae2890445dc09dd72b854093cbe517d4c5
YahorKazak <50490788+YahorKazak@users.noreply.github.com>
2020-06-05 21:35:29 +0200

    Initial commit

---
+++ Projekt_2/E3D.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Projekt_2
+{
+    class E3D
+    {
+      
+       Matrix4x4 matrixProj;
+       Bitmap bm;
+       PictureBox pb;
+
+       Vector3 Camera;
+
+           private void MatrixMnożenia(Matrix4x4 matrixProj,Vector3 x,Vector3 y)
+           {
+            y.X = x.X * matrixProj.M11 + x.Y * matrixProj.M21 + x.Z * matrixProj.M31 + matrixProj.M41;
+            y.Y = x.X * matrixProj.M12 + x.Y * matrixProj.M22 + x.Z * matrixProj.M32 + matrixProj.M42;
+            y.Z = x.X * matrixProj.M13 + x.Y * matrixProj.M23 + x.Z * matrixProj.M33 + matrixProj.M43;
+
+            float width = x.X * matrixProj.M14 + x.Y * matrixProj.M24 + x.Z * matrixProj.M34 + matrixProj.M44;
+
+            if (width != 0.0f)
+            {
+                y.X /= width;
+                y.Y /= width;
+                y.Z /= width;
+            }
+
+           }
+
+           protected class trójkąt
+           {
+            List<Vector3> tlist;
+            public trójkąt(Vector3 first, Vector3 second,Vector3 third)
+            {
+                tlist = new List<Vector3>();
+                tlist.Add(first);
+                tlist.Add(second);
+                tlist.Add(third);
+            }
+           }
+
+        List<trójkąt> Cube;
+        public E3D(PictureBox image)
+        {
+            this.pb = image;
+            Camera = new Vector3(0, 0, 0);
+            Cube = new List<trójkąt>();
+
+            Cube.Add(new trójkąt( new Vector3(0, 0, 0), new Vector3(0, 1, 0),  new Vector3(1, 1, 0)));
+            Cube.Add(new trójkąt(new Vector3(0, 0, 0), new Vector3(1, 1, 0), new Vector3(1, 0, 0)));
+
+            Cube.Add(new trójkąt(new Vector3(1, 0, 0), new Vector3(1, 1, 0), new Vector3(1, 1, 1)));
+            Cube.Add(new trójkąt(new Vector3(1, 0, 0), new Vector3(1, 1, 1), new Vector3(1, 0, 1)));
+
+            Cube.Add(new trójkąt(new Vector3(1, 0, 1), new Vector3(1, 1, 1), new Vector3(0, 1, 1)));
+            Cube.Add(new trójkąt(new Vector3(1, 0, 1), new Vector3(0, 1, 1), new Vector3(0, 0, 1)));
+
+            Cube.Add(new trójkąt(new Vector3(0, 0, 1), new Vector3(0, 1, 1), new Vector3(0, 1, 0)));
+            Cube.Add(new trójkąt(new Vector3(0, 0, 1), new Vector3(0, 1, 0), new Vector3(0, 0, 0)));
+
+            Cube.Add(new trójkąt(new Vector3(0, 1, 0), new Vector3(0, 1, 1), new Vector3(1, 1, 1)));
+            Cube.Add(new trójkąt(new Vector3(0, 1, 0), new Vector3(1, 1, 1), new Vector3(1, 1, 0)));
+
+            Cube.Add(new trójkąt(new Vector3(1, 0, 1), new Vector3(0, 0, 1), new Vector3(0, 0, 0)));
+            Cube.Add(new trójkąt(new Vector3(1, 0, 1), new Vector3(0, 0, 0), new Vector3(1, 0, 0)));
+        }
+
+
+
+       
+    }
+}
---
+++ Projekt_2/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Projekt_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxFirst = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFirst)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxFirst
+            // 
+            this.pictureBoxFirst.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBoxFirst.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxFirst.Name = "pictureBoxFirst";
+            this.pictureBoxFirst.Size = new System.Drawing.Size(1140, 745);
+            this.pictureBoxFirst.TabIndex = 0;
+            this.pictureBoxFirst.TabStop = false;
+            this.pictureBoxFirst.Click += new System.EventHandler(this.pictureBoxFirst_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 745);
+            this.Controls.Add(this.pictureBoxFirst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFirst)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxFirst;
+    }
+}
+
